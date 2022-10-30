70509c2355813050ace3d146a40ffb4de6ccc4b3
unknown <59862637+suzuna@users.noreply.github.com>
2022-10-31 02:51:23 +0900

    Fix: fix text garbling when reading dat files and refactor

--- R/read_dat.R
+++ R/read_dat.R
@@ -1,69 +1,70 @@
-#' Read a dat file of 5ch and return a data.frame.
+#' Read a dat file of 5ch and return a tibble
 #'
-#' @param file a path to a dat file.
-#' @param br_char a string which represents "<br>" (new line) in responses ("res" in Japanese). Default is "[br]".
-#' a string which contains "<" and ">" is not allowed to use.
-#' @param encoding encoding of a file. Default is "Shift-JIS".
+#' @param file a string. a path to a dat file.
+#' @param br_char a string which represents "<br>" (new line) in responses ("res" in Japanese).
+#' It is not allowed to use a string which contains "<" and ">".
+#' @param encoding a string. encoding of a file.
 #'
-#' @return a data.frame which contains this columns: dat_id, thread_title, res_number, name, mail, datetime, id, content.
-#'
-#' Here is a description of each columns.
-#'
-#' dat_id: character. a filename of a dat file.
-#'
-#' thread_title: character. a title of a thread.
-#'
-#' res_number: integer. the number of a response.
-#'
-#' name: character. a name of a response.
-#'
-#' mail: character. a string of mail form.
-#'
-#' datetime: character. a datetime which a response is posted.
-#'
-#' id: character. an id of a response.
-#'
-#' be: character. a be of a response.
-#'
-#' content: character. a content of a response.
-#'
-#' @importFrom magrittr %>%
-#' @importFrom dplyr mutate select filter if_else row_number everything
-#' @importFrom tidyr separate fill
-#' @importFrom stringr str_trim str_extract str_remove_all str_detect str_c str_sub str_replace_all
-
+#' @return a tibble. Here is columns of the tibble.
+#' \itemize{
+#'   \item{dat_id} {character. a filename of a dat file.}
+#'   \item{thread_title} {character. a title of a thread.}
+#'   \item{res_number} {integer. the number of a response.}
+#'   \item{name} {character. a name of a response.}
+#'   \item{mail} {character. a string of mail form.}
+#'   \item{datetime} {character. a datetime which a response is posted.}
+#'   \item{id} {character. an id of a response.}
+#'   \item{be} {character. a be of a response.}
+#'   \item{content} {character. a content of a response.}
+#' }
 #' @examples
 #' \dontrun{
 #' read_dat("foo.dat",br_char="[br]",encoding="Shift-JIS")
 #' }
 #' @export
-read_dat <- function(file,br_char="[br]",encoding="Shift-JIS"){
-  data <- readLines(file,encoding=encoding) %>%
-    str_remove_all("\ufffd")
-  res <- data.frame(tmp=data,stringsAsFactors=FALSE) %>%
-    separate(col=tmp,into=c("name","mail","datetimeidbe","content","thread_title"),sep="<>",fill="right") %>%
-    mutate(thread_title=if_else(thread_title=="",NA_character_,thread_title)) %>%
-    fill(thread_title,.direction="down") %>%
-    mutate(thread_title=str_trim(thread_title)) %>%
-    mutate(res_number=row_number()) %>%
-    mutate(dat_id=str_extract(file,"[^/]*(?=\\.dat$)")) %>%
-    select(dat_id,thread_title,res_number,everything()) %>%
-    mutate(name=str_remove_all(name,"<.*?>")) %>%
-    filter(datetimeidbe!="Over 1000 Thread") %>%
-    separate(col=datetimeidbe,into=c("datetime","idbe"),sep=" (?=ID:)",fill="right") %>%
-    mutate(datetime=if_else(str_detect(datetime,"NY:AN:NY\\.AN"),
-                            str_c(str_sub(datetime,start=1,end=10)," 00:00:00.00"),datetime)) %>%
-    mutate(datetime=str_remove_all(datetime,pattern="\\(.\\)")) %>%
-    separate(col=idbe,into=c("id","be"),sep=" (?=BE:)",fill="right") %>%
-    mutate(id=str_remove_all(id," .*")) %>%
-    mutate(content=str_replace_all(content," <br> ",br_char)) %>%
-    mutate(content=str_remove_all(content,"<.*?>")) %>%
-    mutate(content=str_replace_all(content,c(
-      "&gt;"=">",
-      "&lt;"="<",
-      "&amp;"="&",
-      "&quot;"='"'
-    ))) %>%
-    mutate(content=str_trim(content,side="both"))
+read_dat <- function(file,br_char="[br]",encoding="Shift-JIS") {
+  data_raw_list <- readr::read_lines_raw(file)
+  data <- stringi::stri_encode(data_raw_list,from=encoding) %>%
+    stringr::str_remove_all("\ufffd")
+
+  res <- tibble::tibble(tmp=data) %>%
+    tidyr::separate(col=tmp,into=c("name","mail","datetime_id_be","content","thread_title"),sep="<>",fill="right")
+  res <- res %>%
+    dplyr::mutate(dat_id=stringr::str_extract(file,"[^/]*(?=\\.dat$)")) %>%
+    dplyr::mutate(res_number=dplyr::row_number()) %>%
+    dplyr::mutate(
+      thread_title=dplyr::if_else(thread_title=="",NA_character_,thread_title) %>%
+        stringr::str_trim(side="both")
+    ) %>%
+    tidyr::fill(thread_title,.direction="down") %>%
+    dplyr::mutate(name=stringr::str_remove_all(name,"<.*?>"))
+  res <- res %>%
+    dplyr::filter(datetime_id_be!="Over 1000 Thread")
+  res <- res %>%
+    tidyr::separate(col=datetime_id_be,into=c("datetime","id_be"),sep=" (?=ID:)",fill="right") %>%
+    tidyr::separate(col=id_be,into=c("id","be"),sep=" (?=BE:)",fill="right") %>%
+    dplyr::mutate(id=stringr::str_remove_all(id," .*")) %>%
+    dplyr::mutate(
+      datetime=dplyr::if_else(
+        stringr::str_detect(datetime,"NY:AN:NY\\.AN"),
+        stringr::str_c(stringr::str_sub(datetime,start=1,end=10)," 00:00:00.00"),
+        datetime
+      ) %>%
+        stringr::str_remove_all(pattern="\\(.\\)")
+    )
+  res <- res %>%
+    dplyr::mutate(
+      content=stringr::str_replace_all(content," <br> ",br_char) %>%
+        stringr::str_remove_all("<.*?>") %>%
+        stringr::str_replace_all(c(
+          "&gt;"=">",
+          "&lt;"="<",
+          "&amp;"="&",
+          "&quot;"='"'
+        )) %>%
+        stringr::str_trim(side="both")
+    )
+  res <- res %>%
+    dplyr::relocate(dat_id,thread_title,res_number,name,mail,datetime,id,be,content)
   return(res)
 }
